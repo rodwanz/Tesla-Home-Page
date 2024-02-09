60b8e2208fcd35fd62543d81b90e1c944e89f4d6
rodwanz <wanzervai@gmail.com>
2024-02-09 18:39:24 -0300

    Criando o DefaultOverlayContent e o Model

---
+++ src/components/Model/ModelsWrapper/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+interface Props extends React.HTMLAttributes<HTMLDivElement>{
+  children: React.ReactNode;
+}
+
+const ModelsWrapper: React.FC<Props> = (children) => {
+  return <Container>children</Container>
+}
+
+export default ModelsWrapper
--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 
 import { Container } from './styles'
+import DefaultOverlayContent from '../DefaultOverlayContent'
+import { ModelSection, ModelsWrapper,} from '../Model'
 
 const Page: React.FC = () => {
   return (
     <Container>
       <h1>Page</h1>
+      <ModelsWrapper>
+        <div>
+          <ModelSection
+            modelName="Model One"
+            overlayNode={
+              <DefaultOverlayContent
+                label="Model One"
+                description="Order Online for Delivery"
+              />
+            }
+          />
+        </div>
+      </ModelsWrapper>
     </Container>
   )
 }
